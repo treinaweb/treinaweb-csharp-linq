3a0262ee7aeb6cb2ab659d483bb4e78c5af1b41c
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:39:56 -0300

    05_01_Any_All_Contains

---
+++ 04_04_Expressoes_Lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04_04_Expressoes_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> exibir = informacao => Console.WriteLine($"INFORMAÇÃO: {informacao}");
+            exibir("Testando");
+
+            Func<int, int, int> realizarOperacao = (a, b) => a + b;
+            Console.WriteLine(realizarOperacao(1, 2));
+
+            Predicate<int> verificar = a => a % 2 == 0;
+            Console.WriteLine(verificar(1));
+        }
+    }
+}
---
+++ 05_01_Any_All_Contains/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_01_Any_All_Contains
+{
+    class Program
+    {
+        private static List<int> notas = new List<int> { 500, 600, 700, 800, 900 };
+        static void Main(string[] args)
+        {
+            var any = notas.Any(n => n > 900);
+            var all = notas.All(n => n > 400);
+            var contains = notas.Contains(1000);
+
+            Console.WriteLine("Any: " + any);
+            Console.WriteLine("All: " + all);
+            Console.WriteLine("Contains: " + contains);
+        }
+    }
+}
