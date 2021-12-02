004a27e6d8aefaaa7bc7fb203218202daef79789
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:41:02 -0300

    05_05_Skip

---
+++ 05_05_Skip/Produto.cs
@@ -0,0 +1,14 @@
+namespace _05_05_Skip
+{
+    public class Produto
+    {
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    
+        public override string ToString()
+        {
+            return $"{ Descricao } ({ Categoria }): { Preco }";
+        }
+    }
+}
---
+++ 05_05_Skip/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_05_Skip
+{
+    class Program
+    {
+        private static List<Produto> produtos = new List<Produto>
+        {
+            new Produto { Descricao = "Arroz", Categoria = "Cereais", Preco = 4.89m },
+            new Produto { Descricao = "Feijão", Categoria = "Cereais", Preco = 8.19m },
+            new Produto { Descricao = "Detergente", Categoria = "Limpeza", Preco = 2.29m },
+            new Produto { Descricao = "Desinfetante", Categoria = "Limpeza", Preco = 3.99m }
+        };
+
+        static void Main(string[] args)
+        {
+            var resultado = produtos.Skip(3).Take(3);
+
+            foreach (var r in resultado)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
