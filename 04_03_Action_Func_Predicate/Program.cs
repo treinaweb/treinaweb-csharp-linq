0e5300192f2c1f96a487c7aacc4485aae82dad44
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:39:25 -0300

    04_03_Action_Func_Predicate

---
+++ 04_03_Action_Func_Predicate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04_02_Action_Func_Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> exibir = ExibirInformacao;
+            exibir("Testando");
+
+            Func<int, int, int> realizarOperacao = Somar;
+            Console.WriteLine(realizarOperacao(1, 2));
+
+            Predicate<int> verificar = EhPar;
+            Console.WriteLine(verificar(1));
+        }
+
+        static void ExibirInformacao(string informacao)
+        {
+            Console.WriteLine($"INFORMAÇÃO: {informacao}");
+        }
+
+        static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        static bool EhPar(int a)
+        {
+            return a % 2 == 0;
+        }
+    }
+}
