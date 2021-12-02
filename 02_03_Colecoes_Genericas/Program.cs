ef2fd13604a1bced474ab3bd58d8cb2373bcfb34
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:33:00 -0300

    02_03_Colecoes_Genericas

---
+++ 02_03_Colecoes_Genericas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_03_Colecoes_Genericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
