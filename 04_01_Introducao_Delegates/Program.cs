0b1ea152fd1fbf33e3dc475141b432462226e68d
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:36:31 -0300

    04_01_Introducao_Delegates

---
+++ 04_01_Introducao_Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04_01_Introducao_Delegates
+{
+    delegate void ExibirMensagem(string texto);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExibirMensagem exibir = ExibirInformacao;
+
+            exibir("Testando");
+
+            exibir = ExibirErro;
+
+            exibir("Testando");
+        }
+
+        static void ExibirInformacao(string informacao)
+        {
+            Console.WriteLine($"INFORMAÇÃO: {informacao}");
+        }
+        
+        static void ExibirErro(string informacao)
+        {
+            Console.WriteLine($"ERRO: {informacao}");
+        }
+    }
+}
