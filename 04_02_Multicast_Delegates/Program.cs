0d784fb58dec032554e2c08611c34d08b6781eb0
Joel Rodrigues <joel.lima@engenhos.net>
2021-12-01 21:36:47 -0300

    04_02_Multicast_Delegates

---
+++ 04_02_Multicast_Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04_02_Multicast_Delegates
+{
+    public delegate void Notificacao(string mensagem);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notificacao notificacao = new Notificacao(NotificarPorEmail);
+            notificacao += NotificarPorWhatsApp;
+            notificacao += NotificarPorSMS;
+
+            // notificacao("TESTE");
+            //ou
+            notificacao.Invoke("TESTE");
+
+            notificacao -= NotificarPorEmail;
+            notificacao.Invoke("TESTE2");
+        }
+
+        static void NotificarPorEmail(string mensagem)
+        {
+            Console.WriteLine($"Enviando por e-mail: { mensagem }");
+        }
+
+        static void NotificarPorWhatsApp(string mensagem)
+        {
+            Console.WriteLine($"Enviando por WhatsApp: { mensagem }");
+        }
+
+        static void NotificarPorSMS(string mensagem)
+        {
+            Console.WriteLine($"Enviando por SMS: { mensagem }");
+        }
+    }
+}
